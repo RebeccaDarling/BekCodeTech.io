387b5869749cda8b3c7920bbad4d436eb871d33e
RebeccaDarling <145950129+RebeccaDarling@users.noreply.github.com>
2023-10-20 13:26:49 -0500

    Delete folder directory

--- folder/homescript.js
+++
@@ -1,4 +0,0 @@
-function toggleSideBar() {
-    document.getElementById('sidebar').classList.toggle('active');
-  }
-  
--- folder/script.js
+++
@@ -1,8 +0,0 @@
-function toggleSidebar() {
-    const sidebar = document.getElementById("mySidebar");
-    if (sidebar.style.width === "250px") {
-        sidebar.style.width = "0";
-    } else {
-        sidebar.style.width = "250px";
-    }
-}
