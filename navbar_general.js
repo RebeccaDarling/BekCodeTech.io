1900dc0c6be43d46de291f8c796ce55316d0941e
RebeccaDarling <145950129+RebeccaDarling@users.noreply.github.com>
2023-11-04 23:17:12 -0500

    Update navbar_general.js

--- navbar_general.js
+++ navbar_general.js
@@ -4,3 +4,11 @@ const mobileMenu = document.getElementById('mobile-menu');
 menuIcon.addEventListener('click', () => {
   mobileMenu.style.display = mobileMenu.style.display === 'block' ? 'none' : 'block';
 });
+
+const cards = document.querySelectorAll('.card');
+
+cards.forEach(card => {
+  card.addEventListener('click', () => {
+    card.classList.toggle('is-flipped');
+  });
+});
